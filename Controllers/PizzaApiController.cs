48fe3837254b9df1d5f929efe0fa798df22dc94c
OCTAVIO ARFA TENENBERG <47292048@ort>
2023-03-17 11:51:58 -0300

    b

--- Controllers/PizzaApiController.cs
+++ Controllers/PizzaApiController.cs
@@ -10,30 +10,53 @@ public class PizzaApiController : ControllerBase
     [HttpGet]
     public IActionResult GetAll()
     {
+        List<Pizza> l;
 
+        l = BD.GetAll();
+        return Ok(l);
     }
 
     [HttpGet("{id}")]
     public IActionResult GetById(int id)
     {
-
+        Pizza p = BD.GetById(id);
+        return p == null ? NotFound() : Ok(p);
     }
 
     [HttpPost]
     public IActionResult Create(Pizza pizza)
     {
-
+        int n;
+        n = BD.Create(pizza);
+        return CreatedAtAction(nameof(Create), pizza);
     }
 
     [HttpPut("{id}")]
     public IActionResult Update(int id, Pizza pizza)
     {
-
+        if(id != pizza.Id){
+            return BadRequest();
+        }
+        if(BD.GetById(id) == null){
+            return NotFound();
+        }
+        return BD.Update(pizza) > 0 ? Ok(pizza) : NotFound();
     }
 
     [HttpDelete("{id}")]
     public IActionResult DeleteByID(int id)
     {
-
+        int n;
+        Pizza p;
+        p = BD.GetById(id);
+        if(p == null){
+            return NotFound();
+        }
+        n = BD.DeleteByID(id);
+        if(n > 0){
+            return Ok(p);
+        }else{
+            return NotFound();
+        }
     }
 }
--- Models/BD.cs
+++ Models/BD.cs
@@ -1,6 +1,68 @@
+using Dapper;
+using System.Data.SqlClient;
 namespace PizzaApi.Models;
 
-public class BD
+
+public static class BD
 {
-    
+    private static string connectionStr = "Server=A-PHZ2-AMI-020;Database=PizzaDB;Trusted_Connection=True;";
+
+    public static List<Pizza> GetAll()
+    {
+        List<Pizza> l = new List<Pizza>();
+        string query;
+
+        using(SqlConnection db = new SqlConnection(connectionStr)){
+            query = "SELECT * FROM Pizzas";
+            l = db.Query<Pizza>(query).ToList();
+        }
+        return l;
+    }
+    public static Pizza GetById(int id)
+    {
+        string query;
+        Pizza p;
+
+        using(SqlConnection db = new SqlConnection(connectionStr)){
+            query = "SELECT * FROM Pizzas WHERE Id=@pId";
+            p = db.QueryFirstOrDefault<Pizza>(query,new{pId = id});
+        }
+        return p;
+    }
+
+    public static int Create(Pizza p)
+    {
+        string query;
+        int n;
+
+        using(SqlConnection db = new SqlConnection(connectionStr)){
+            query = "INSERT INTO Pizzas (Nombre, LibreGluten, Importe, Descripcion) VALUES (@pNombre, @pLibreGluten, @pImporte, @pDescripcion)";
+            n = db.Execute(query, new{pNombre = p.Nombre, pLibreGluten = p.LibreGluten, pImporte = p.Importe, pDescripcion = p.Descripcion});
+        }
+        return n;
+    }
+
+    public static int Update(Pizza p)
+    {
+        string query;
+        int n;
+
+        using(SqlConnection db = new SqlConnection(connectionStr)){
+            query = "UPDATE Pizzas SET Nombre = @pNombre, LibreGluten = @pLibreGluten, Importe = @pImporte, Descripcion = @pDescripcion WHERE Id = @pId";
+            n = db.Execute(query, new{pNombre = p.Nombre, pLibreGluten = p.LibreGluten, pImporte = p.Importe, pDescripcion = p.Descripcion, pId = p.Id});
+        }
+        return n;
+    }
+
+    public static int DeleteByID(int id)
+    {
+        string query;
+        int n;
+
+        using(SqlConnection db = new SqlConnection(connectionStr)){
+            query = "DELETE FROM Pizzas WHERE Id = @pId";
+            n = db.Execute(query, new{pId = id});
+        }
+        return n;
+    }
 }
